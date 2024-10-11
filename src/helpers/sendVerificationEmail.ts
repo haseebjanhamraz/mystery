656404698e04b99fe3f9d56f84312020a759ff4d
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-10-12 01:17:07 +0500

    "Added next-auth dependency and implemented authentication context, updated package-lock.json and package.json, modified .env file, and updated layout and sendVerificationEmail files"

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'has@gmail.com',
+            from: 'haseeb@gmail.com',
             to: email,
             subject: 'SAPLAY - Verification Code',
             react: VerificationEmail({ username, otp: verifyCode })
